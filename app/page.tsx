c8fc0c15e955209ad63958c14da467ff6bdcab24
Rohit Reddy Gangwar <rohitreddygangwar@gmail.com>
2024-08-13 02:27:08 +0530

    Fix build error and add loading state

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Search } from "@/components/search";
 import { useCart } from "@/contexts/cart-context";
 import { useProducts } from "@/hooks/use-products";
 import Image from "next/image";
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import toast from "react-hot-toast";
 import { LuMinus, LuPlus, LuShoppingCart, LuStar } from "react-icons/lu";
 
@@ -30,56 +30,70 @@ const Page = () => {
 				<Cart />
 			</nav>
 			<h1 className="font-bold text-4xl">Products</h1>
-			<Search onSearch={setSearch} />
+			<Suspense>
+				<Search onSearch={setSearch} />
+			</Suspense>
 			<div className="mx-auto flex flex-wrap gap-4">
-				{filteredProducts.map((item, i) => (
-					<div className="flex w-64 flex-col gap-4 rounded-md bg-white p-6 shadow-md max-sm:w-full max-sm:flex-row" key={item.id}>
-						<Image className="aspect-square w-full self-center max-sm:w-2/5" src={item.image} alt={item.title} width={200} height={200} />
-						<div className="flex flex-col space-y-2 max-sm:w-40">
-							<h3 className="line-clamp-2 font-semibold text-gray-600">{item.title}</h3>
-							<div className="flex items-center gap-1">
-								{new Array(5).fill(null).map((_, idx) => (
-									<LuStar
-										className={`border-none stroke-yellow-600 ${idx < (i % 3 === 0 ? 5 : 4) && "fill-yellow-600"}`}
-										// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-										key={idx}
-									/>
-								))}
-								<span className="ml-1 font-semibold text-gray-600 text-sm">{noOfRatings[i]}</span>
-							</div>
-							<div className="flex items-center justify-between">
-								<span className="font-bold text-lg">&#36;{item.price}</span>
-								{i % 3 === 0 && (
-									<span className="rounded border-green-500 bg-green-300/50 px-1 py-0.5 font-bold text-green-600 text-xs">
-										{i % 2 === 0 ? "30% off" : "50% off"}
-									</span>
+				{products.length > 0 ? (
+					filteredProducts.map((item, i) => (
+						<div className="flex w-64 flex-col gap-4 rounded-md bg-white p-6 shadow-md max-sm:w-full max-sm:flex-row" key={item.id}>
+							<Image
+								className="aspect-square w-full self-center max-sm:w-2/5"
+								src={item.image}
+								alt={item.title}
+								width={200}
+								height={200}
+							/>
+							<div className="flex flex-col space-y-2 max-sm:w-40">
+								<h3 className="line-clamp-2 font-semibold text-gray-600">{item.title}</h3>
+								<div className="flex items-center gap-1">
+									{new Array(5).fill(null).map((_, idx) => (
+										<LuStar
+											className={`border-none stroke-yellow-600 ${idx < (i % 3 === 0 ? 5 : 4) && "fill-yellow-600"}`}
+											// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+											key={idx}
+										/>
+									))}
+									<span className="ml-1 font-semibold text-gray-600 text-sm">{noOfRatings[i]}</span>
+								</div>
+								<div className="flex items-center justify-between">
+									<span className="font-bold text-lg">&#36;{item.price}</span>
+									{i % 3 === 0 && (
+										<span className="rounded border-green-500 bg-green-300/50 px-1 py-0.5 font-bold text-green-600 text-xs">
+											{i % 2 === 0 ? "30% off" : "50% off"}
+										</span>
+									)}
+								</div>
+								{cart[item.id] ? (
+									<div className="flex h-10 w-full items-center justify-evenly rounded-md border border-gray-300 *:h-full">
+										<button className="p-2" onClick={() => updateItem(item.id, cart[item.id] - 1)}>
+											<LuMinus className="stroke-[4]" />
+										</button>
+										<span className="my-auto flex items-center text-center font-semibold">{cart[item.id]}</span>
+										<button className="p-2" onClick={() => updateItem(item.id, cart[item.id] + 1)}>
+											<LuPlus className="stroke-[4]" />
+										</button>
+									</div>
+								) : (
+									<button
+										type="button"
+										className="flex h-10 items-center justify-center gap-2 rounded bg-blue-500 px-4 font-semibold text-white hover:bg-blue-700"
+										onClick={() => {
+											addItem(item.id);
+											toast.success("Added item to cart", { style: { fontWeight: "600" } });
+										}}
+									>
+										Add to cart <LuShoppingCart className="size-4" />
+									</button>
 								)}
 							</div>
-							{cart[item.id] ? (
-								<div className="flex h-10 w-full items-center justify-evenly rounded-md border border-gray-300 *:h-full">
-									<button className="p-2" onClick={() => updateItem(item.id, cart[item.id] - 1)}>
-										<LuMinus className="stroke-[4]" />
-									</button>
-									<span className="my-auto flex items-center text-center font-semibold">{cart[item.id]}</span>
-									<button className="p-2" onClick={() => updateItem(item.id, cart[item.id] + 1)}>
-										<LuPlus className="stroke-[4]" />
-									</button>
-								</div>
-							) : (
-								<button
-									type="button"
-									className="flex h-10 items-center justify-center gap-2 rounded bg-blue-500 px-4 font-semibold text-white hover:bg-blue-700"
-									onClick={() => {
-										addItem(item.id);
-										toast.success("Added item to cart", { style: { fontWeight: "600" } });
-									}}
-								>
-									Add to cart <LuShoppingCart className="size-4" />
-								</button>
-							)}
 						</div>
+					))
+				) : (
+					<div className="flex size-full items-center justify-center font-semibold text-gray-500 text-lg">
+						<p>Loading...</p>
 					</div>
-				))}
+				)}
 			</div>
 		</main>
 	);
