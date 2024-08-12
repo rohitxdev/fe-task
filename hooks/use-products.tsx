c0cfd88ab4a5c4d2cb722f1347bc6280427e2260
Rohit Reddy Gangwar <rohitreddygangwar@gmail.com>
2024-08-12 22:18:33 +0530

    Add fallback data

--- hooks/use-products.tsx
+++ hooks/use-products.tsx
@@ -1,17 +1,13 @@
 import { useEffect, useState } from "react";
 import { z } from "zod";
+import fallBackProducts from "./products.json";
 
 const productSchema = z.object({
 	id: z.number(),
 	title: z.string().min(1),
 	price: z.number(),
 	description: z.string().min(1),
-	category: z.string().min(1),
 	image: z.string().min(1),
-	rating: z.object({
-		rate: z.number(),
-		count: z.number(),
-	}),
 });
 
 const productsSchema = z.array(productSchema);
@@ -19,9 +15,13 @@ const productsSchema = z.array(productSchema);
 type Product = z.infer<typeof productSchema>;
 
 const fetchProducts = async (callback: (products: Product[]) => void) => {
-	const response = await fetch("https://fakestoreapi.com/products");
-	const data = await response.json();
-	callback(productsSchema.parse(data));
+	try {
+		const response = await fetch("https://fakestoreapi.in/api/products?limit=30");
+		const data = await response.json();
+		callback(productsSchema.parse(data.products));
+	} catch (error) {
+		callback(fallBackProducts);
+	}
 	return;
 };
 
@@ -36,6 +36,7 @@ export const useProducts = () => {
 		}
 
 		fetchProducts((data) => {
+			console.log(data);
 			setProducts(data);
 			sessionStorage.setItem("products", JSON.stringify(data));
 		});
